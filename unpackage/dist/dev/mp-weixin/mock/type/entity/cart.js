8f6d89be3ce5e1a1af936160ea75ff878bb65203
mee <1004460703@qq.com>
2023-02-21 17:50:50 +0800

    clear cache

--- unpackage/dist/dev/mp-weixin/mock/type/entity/cart.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var CouponTypeType = /* @__PURE__ */ ((CouponTypeType2) => {
-  CouponTypeType2[CouponTypeType2["ANY_DISCOUNT"] = 0] = "ANY_DISCOUNT";
-  CouponTypeType2[CouponTypeType2["FULL_DISCOUNT"] = 1] = "FULL_DISCOUNT";
-  return CouponTypeType2;
-})(CouponTypeType || {});
-exports.CouponTypeType = CouponTypeType;
--- unpackage/dist/dev/mp-weixin/mock/type/entity/goods.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var ProductChannel = /* @__PURE__ */ ((ProductChannel2) => {
-  ProductChannel2[ProductChannel2["SELF"] = 0] = "SELF";
-  ProductChannel2[ProductChannel2["OVERSEAS"] = 1] = "OVERSEAS";
-  ProductChannel2[ProductChannel2["NON_PROPRIETARY"] = 2] = "NON_PROPRIETARY";
-  return ProductChannel2;
-})(ProductChannel || {});
-exports.ProductChannel = ProductChannel;
